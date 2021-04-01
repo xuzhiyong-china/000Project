fec395b4dbb8698e343702f897eff444d6c53451
xuzhiyong-china <386651464@qq.com>
2021-04-01 20:34:27 +0800

    增加文件夹与文档。

---
+++ SI_Documents/github.c
@@ -0,0 +1,7 @@
+先安装git，在安装可视化工具。
+克隆时，需要先模仿这个网址登录git账号
+https://blog.csdn.net/xupengbo527/article/details/90290495
+然后按照这个操作可视化工具
+https://blog.csdn.net/weixin_44780625/article/details/105150896
+
+
