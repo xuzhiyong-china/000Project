06c42ac18808d022cf70bd83e0e66f19e6276d86
xuzhiyong-china <386651464@qq.com>
2021-04-01 21:36:04 +0800

    只保存SI的文件，不保存工程

--- SI_Documents/github.c
+++ SI_Documents/github.c
@@ -4,4 +4,7 @@ https://blog.csdn.net/xupengbo527/article/details/90290495
 然后按照这个操作可视化工具
 https://blog.csdn.net/weixin_44780625/article/details/105150896
 
+变更的信息上传上去后，要等一会才会更新。
+
+Github桌面选择“文件夹“即可看到log
 
